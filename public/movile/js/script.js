2d75b5afa18ff10672445be50c4494294cc042b5
Bulletox <javiersernahuertas@gmail.com>
2024-04-12 11:22:56 +0200

    Actualización de enlaces de estilos y scripts en el archivo comida.html

--- public/movile/js/script.js
+++ public/movile/js/script.js
@@ -1,11 +1,35 @@
+
+// Variable global para almacenar la ID de la instancia
+let globalIdInstancia = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const queryParams = new URLSearchParams(window.location.search);
+    globalIdInstancia = queryParams.get('idInstancia'); // Almacena la ID globalmente
+
+    if (globalIdInstancia) {
+        console.log("ID de la Instancia:", globalIdInstancia);
+    } else {
+        console.log("No se encontró ID de instancia en la URL.");
+    }
+});
+
+
 function visuaComida() {
-    location.href = "visuaComida.html"
+    if (globalIdInstancia) {
+        location.href = `visuaComida.html?id=${globalIdInstancia}`;
+    } else {
+        console.log("Error: No hay ID de instancia disponible.");
+    }
+}
+
+function verCarro() {
+    if (globalIdInstancia) {
+        location.href = `carrito.html?id=${globalIdInstancia}`;
+    } else {
+        console.log("Error: No hay ID de instancia disponible.");
+    }
 }
 
 function goBack() {
     window.history.go(-1);
-}
-
-function verCarro(){
-    location.href = "carrito.html"
 }
