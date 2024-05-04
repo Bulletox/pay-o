e90d14140e2a9e5cb65e9d39ae1fee9627d101f0
DonHuevo119 <1193834006huevo@gmail.com>
2024-05-04 23:54:08 +0200

    He solucionado algunos errores y he hecho una mega mejora en la landing page. He mejorado tambien los sidebar del panel de control.

--- public/panelcontrol/js/script.js
+++ public/panelcontrol/js/script.js
@@ -53,6 +53,10 @@ function irAyuda() {
 function irmesas() {
     location.href = "/panelcontrol/gestionmesas.html"
 }
+
+function irIndex(){
+    location.href = "/panelcontrol/index.html"
+}
 // Imagen
 
 function handleClick() {
@@ -138,80 +142,3 @@ function emergenteInfo() {
         emergente = true
     }
 }
-
-// Obtener elementos
-const videoTitle = document.getElementById('video');
-
-function comprobarTexto() {
-    let texto = document.querySelector('#video').textContent
-    if (texto === "pedido.") {
-        eraseVideo()
-    } else {
-        eraseVideo2()
-    }
-}
-
-function eraseVideo() {
-    let text = document.querySelector('#video').textContent
-    if (text.length > 0) {
-        let contenido = text.slice(0, -1); // Elimina el último carácter
-        document.querySelector('#video').innerText = contenido
-        setTimeout(eraseVideo, 70); // Llama a la función después de 100 milisegundos
-    } else {
-        revealOediv(); // Cuando el texto 'video' está vacío, muestra 'oediv' gradualmente
-    }
-
-}
-
-function eraseVideo2() {
-    let text = document.querySelector('#video').textContent
-    if (text.length > 0) {
-        let contenido = text.slice(0, -1); // Elimina el último carácter
-        document.querySelector('#video').innerText = contenido
-        setTimeout(eraseVideo2, 70); // Llama a la función después de 100 milisegundos
-    } else {
-        revealOediv2();
-    }
-
-}
-
-// Función para introducir gradualmente el texto 'oediv'
-function revealOediv() {
-
-    let text = "pago.";
-    let index = 0;
-
-    function addNextLetter() {
-        if (index <= text.length) {
-            videoTitle.innerText = text.slice(0, index); // Mostrar el texto hasta el índice actual
-            index++;
-            setTimeout(addNextLetter, 50); // Llama a la función después de 100 milisegundos
-        } else {
-            setTimeout(comprobarTexto, 1500); // Espera 1 segundo antes de iniciar el borrado
-        }
-    }
-
-    addNextLetter(); // Comienza a agregar letras
-}
-
-function revealOediv2() {
-    let text = "pedido.";
-    let index = 0;
-
-    function addNextLetter() {
-        if (index <= text.length) {
-            videoTitle.innerText = text.slice(0, index); // Mostrar el texto hasta el índice actual
-            index++;
-            setTimeout(addNextLetter, 50); // Llama a la función después de 100 milisegundos
-        } else {
-            setTimeout(comprobarTexto, 1500); // Espera 1 segundo antes de iniciar el borrado
-        }
-    }
-
-    addNextLetter(); // Comienza a agregar letras
-}
-
-// Función para mostrar un alert después de mostrar 'oediv'
-
-// Llamar a la función para comenzar a borrar el texto
-setTimeout(comprobarTexto, 1500); // Espera 1 segundo antes de iniciar el borrado
